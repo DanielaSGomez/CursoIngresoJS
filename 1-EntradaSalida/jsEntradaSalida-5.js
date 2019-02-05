aea5ab3dacbb172b81236985477bdef4def1c822
DanielaSGomez <47353948+DanielaSGomez@users.noreply.github.com>
2019-02-05 12:08:34 -0300

    Primer commit propio
    Ejercicios del 1 al 5

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,14 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var nombre = prompt("Ingrese su nombre");
+	var edad = prompt ("Ingrese su edad");
+
+	elNombre.value=nombre;
+	laEdad.value=edad;
+
+	alert("Usted se llama " + nombre + " y tiene " + edad + " años.");
+
+
 }
 
