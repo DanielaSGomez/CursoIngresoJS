d23c58d2a4497a9e8fa932d1a9958222a1c975ad
unknown <danielagomez992@hotmail.com>
2019-02-12 20:05:05 -0300

    IF Resueltos

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+	numeroRandom = Math.floor(Math.random() * 10) + 1;
+
+
+	if (numeroRandom>=9&&numeroRandom<=10) {
+			alert("Excelente, Nota: " + numeroRandom);
+		} else if (numeroRandom>=4) {
+			alert("Aprobo, Nota: " + numeroRandom);
+		}else{
+			alert("Vamos, la proxima se puede, Nota: " + numeroRandom);
+	}
+
 
 }//FIN DE LA FUNCIÓN
