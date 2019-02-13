d75208102064ee4dcd3ac02ffd206a32d7d79090
DanielaSGomez <danielagomez992@hotmail.com>
2019-02-13 09:21:03 -0300

    Parcialito2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,11 +1,26 @@
 function mostrar()
 {
-  var name;
+ /*var name;
   var adress;
 
   name = elNombre.value;
   adress = laLocalidad.value;
 
-  alert("Usted es " + name + " y vive en la localidad de " + adress +".");
+  alert("Usted es " + name + " y vive en la localidad de " + adress +".");*/
+
+var precioIngresado;
+var precioConRecargo;
+var precioFinal;
+
+precioIngresado = elNombre.value;
+precioIngresado = parseInt(precioIngresado);
+
+precioConRecargo = (precioIngresado*10)/100;
+
+precioFinal = precioIngresado + precioConRecargo;
+
+alert("El precio del producto en efectivo es: $"+ precioIngresado+", con tarjeta tiene un recargo del 10% que seria $:" + precioConRecargo + ", costandole final $:" + precioFinal);
+
+
 
 }
