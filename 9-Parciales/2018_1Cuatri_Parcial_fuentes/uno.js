2d1a4503cec64bc5c34491dfc7ebea20bd46eaa2
alumno <alumno>
2019-02-13 09:08:55 -0300

    Parcialito

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,7 +1,7 @@
 
 function mostrar()
 {
-	var ancho;
+	/*var ancho;
 	var largo;
 	var perimetroResultado;
 
@@ -13,6 +13,29 @@ function mostrar()
 
 	perimetroResultado = (ancho*2)+(largo*2);
 
-	alert("El perimetro del rectangulo es "+ perimetroResultado);
+	alert("El perimetro del rectangulo es "+ perimetroResultado);*/
+
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var sumaEdad;
+
+	nombre1 = prompt("Persona 1: Ingrese su nombre");
+	edad1 = prompt("Persona 1: Ingrese su edad");
+	edad1 = parseInt(edad1);
+	nombre2 = prompt("Persona 2: Ingrese su nombre");
+	edad2 = prompt("Persona 2: Ingrese su edad");
+	edad2 = parseInt(edad2);
+
+	sumaEdad = edad1+edad2;
+
+	alert("Ustedes son " + nombre1 + " y " + nombre2 + ", sus edades son " + edad1 + " y " + edad2 + ", y la suma de sus edades es " + sumaEdad + ".");
+
+
+
+
+
 
 }
+
