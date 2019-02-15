47c5bf69fb258150ae6d4b60afcd5f2a52eff37c
unknown <danielagomez992@hotmail.com>
2019-02-15 15:45:31 -0300

    TP Adivina el numero1

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,18 +9,38 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 var contadorIntentos;
+var numeroIngresado;
+var mensaje;
+
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+
+	numeroSecreto= Math.floor(Math.random() * 100) + 1;
+	contadorIntentos=0;
+	console.log(numeroSecreto);
 		//alert(numeroSecreto );
 	
-
 }
 
 function verificar()
 {
 	
-	
+	numeroIngresado=numero.value;
+	numeroIngresado=parseInt(numeroIngresado);
+	contadorIntentos=contadorIntentos+1;
+	intentos.value=contadorIntentos;
+
+	if (numeroIngresado==numeroSecreto) {
+		mensaje="Usted es un ganador!!! y en solo "+ contadorIntentos + " intentos.";
+	}else if (numeroIngresado>numeroSecreto) {
+		mensaje="Se paso";
+	} else{
+		mensaje="Te falta";
+	}
+
+	alert(mensaje);
+
+
 }
