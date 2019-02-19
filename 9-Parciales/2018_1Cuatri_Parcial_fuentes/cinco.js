7938785c1d5278ba01f722b334a5dabc787d79af
alumno <alumno>
2019-02-19 12:12:36 -0300

    parcial 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+	var planetaIngresado;
+
+	planetaIngresado=prompt("Ingrese un planeta del Sistema Solar","Ej:mercurio,venus,etc");
+
+	var mensaje;
+
+	switch(planetaIngresado)
+	{
+		case "tierra":
+			mensaje="Acá vivimos";
+			break;
+
+		case "venus":
+		case "mercurio":
+			mensaje="Acá hace más calor";
+			break;
+
+		case
+
+
+
+
+
+
+
+
+	}
+
+
 
 }
