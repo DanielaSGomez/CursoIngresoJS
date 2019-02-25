7681b0a3534c4495228b10268f76ced19490d062
unknown <danielagomez992@hotmail.com>
2019-02-25 19:44:24 -0300

    Ej while terminados

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,16 +3,68 @@ function mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
-	
+	var numeroIngresado;
+	var contadorPositivo = 0;
+	var contadorNegativo = 0;
+	var sumaPositivos = 0;
+	var sumaNegativos = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
 	var respuesta="si";
 
 	while(respuesta!="no")
 	{
-		
+		numeroIngresado = prompt("Ingrese un numero.");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		if (numeroIngresado>0) //es positivo
+		{
+			sumaPositivos = sumaPositivos + numeroIngresado;
+			contadorPositivo++;
+		}
+		else
+		{
+			if (numeroIngresado<0) //es negativo
+			{
+				sumaNegativos = sumaNegativos + numeroIngresado;
+				contadorNegativo++;
+			}
+			else//es cero
+			{
+				contadorCeros++;
+			}// (numeroIngresado<0)
+
+
+		}//(numeroIngresado>0) 
 	
+
+		if (numeroIngresado%2 == 0) //es par
+		{
+			contadorPares++;
+		}
+
+
+		
+		respuesta = prompt("Desea continuar? si/no");
+
+
 	}
+	promedioPositivos = sumaPositivos/contadorPositivo;
+		promedioNegativos = sumaNegativos/contadorNegativo;
+		diferencia = sumaPositivos - sumaNegativos;
 
 
-
+document.write("Suma de negativos: " + sumaNegativos+ "<br>"+
+"Suma de positivos: " + sumaPositivos+"<br>"+
+"Cantidad de positivos: " +  contadorPositivo+"<br>"+
+"Cantidad de negativos: " + contadorNegativo+"<br>"+
+"Cantidad de ceros: " + contadorCeros+"<br>"+
+"Cantidad de numeros pares: " + contadorPares+"<br>"+
+"Promedio positivos: " + promedioPositivos+"<br>"+
+"Promedio negativos: " + promedioNegativos+"<br>"+
+"Diferencia: " + diferencia);
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,16 +5,48 @@ function mostrar()
 	// declarar variables
 	var numeroIngresado;
 	
+	var max;
+	var min;
 	
 	var respuesta='si';
 
-	while(respuesta!='no')
+	while(respuesta!="no")
 	{
-		
+		contador++;
+		numeroIngresado = prompt("Ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
 	
+
+		if (contador==1) //si el contador vale 1,los maximos y minimos van a valer el num ingresado xq no hay mas, a partir de aca se van a tomar como referencia este if para determinar los otros maximos y minimos
+		{
+			max = numeroIngresado;
+			min = numeroIngresado;
+			contador++;
+
+		}
+		else
+		{
+			if (numeroIngresado>max) 
+			{
+				max = numeroIngresado;
+			}
+        	else 
+        	{
+        		min = numeroIngresado;
+        	}
+		}
+
+		
+
+
+
+		respuesta = prompt("Desea continuar? si/no.");
+
+
 	}
 
-
+maximo.value=max;
+minimo.value=min;
 
 
 }//FIN DE LA FUNCIÓN
